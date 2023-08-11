690cf46703c70af79a7646dbdcdb06573aaf8b98
ViktorVanHulle <viktor.van.hulle@gmail.com>
2023-08-11 18:56:21 +0200

    vallende objecten

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 import * as THREE from "three";
 import * as CANNON from "cannon-es";
+import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI();
 
 //Sizes
 const sizes = {
@@ -23,7 +29,8 @@ orbit.update();
 //Scene
 const scene = new THREE.Scene();
 
-//Object
+//Objects
+// platform
 const groundGeometry = new THREE.PlaneGeometry(30, 30);
 const groundMaterial = new THREE.MeshBasicMaterial({
   color: 0xffffff,
@@ -33,26 +40,75 @@ const groundMaterial = new THREE.MeshBasicMaterial({
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 scene.add(groundMesh);
 
+//bowlingbal
+const bowlingBallGeometry = new THREE.SphereGeometry(2);
+const bowlingBallMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const bowlingballMesh = new THREE.Mesh(
+  bowlingBallGeometry,
+  bowlingBallMaterial
+);
+scene.add(bowlingballMesh);
+
+//citroen
+const lemonGeometry = new THREE.SphereGeometry(1);
+const lemonMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  wireframe: true,
+});
+const lemonMesh = new THREE.Mesh(lemonGeometry, lemonMaterial);
+scene.add(lemonMesh);
+
 // Fysieke wereld
 const world = new CANNON.World({
   gravity: new CANNON.Vec3(0, -9.81, 0),
 });
 
+// platform
 const groundBody = new CANNON.Body({
   shape: new CANNON.Plane(),
-  // mass: 0,
-  // type: CANNON.Body.STATIC,
+  mass: 0,
+  type: CANNON.Body.STATIC,
 });
 world.addBody(groundBody);
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 
+//bowlingbal
+const bowlingBallBody = new CANNON.Body({
+  shape: new CANNON.Sphere(2),
+  mass: 7.26, //een standaard bowling bal weegt 7.26kg
+  position: new CANNON.Vec3(1, 20, 0),
+});
+world.addBody(bowlingBallBody);
+
+const lemonBody = new CANNON.Body({
+  shape: new CANNON.Sphere(1),
+  mass: 0.58, //een standaard citroen weegt 58gr
+  position: new CANNON.Vec3(0, 15, 0),
+});
+world.addBody(lemonBody);
+
 const timeStep = 1 / 60;
+const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
 function animate() {
-  world.step(timeStep);
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+  world.step(timeStep, deltaTime, 3);
 
+  //platform
   groundMesh.position.copy(groundBody.position);
   groundMesh.quaternion.copy(groundBody.quaternion);
+  //bowling bal
+  bowlingballMesh.position.copy(bowlingBallBody.position);
+  bowlingballMesh.quaternion.copy(bowlingBallBody.quaternion);
+  //lemon
+  lemonMesh.position.copy(lemonBody.position);
+  lemonMesh.quaternion.copy(lemonBody.quaternion);
 
   renderer.render(scene, camera);
 }
